01e0d33bfbeb03aecb26088fb55b7fa9b9c3d285
aries0d0f <aries0d0f@gmail.com>
2022-07-22 13:52:20 +0800

    build: Setup icon plugin and resolver.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import { join } from 'path';
-import Vue from '@vitejs/plugin-vue'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +19,38 @@ export default defineConfig({
       reactivityTransform: true
     }),
     splitVendorChunkPlugin(),
+    AutoImport({
+      resolvers: [
+        IconsResolver({
+          prefix: 'icon',
+          alias: {
+            fab: 'fa-brands',
+            fas: 'fa-solid',
+            far: 'fa-regular'
+          }
+        })
+      ]
+    }),
+    Components({
+      dts: true,
+      resolvers: [
+        IconsResolver({
+          prefix: 'icon',
+          alias: {
+            fab: 'fa-brands',
+            fas: 'fa-solid',
+            far: 'fa-regular'
+          },
+          customCollections: ['agh']
+        })
+      ]
+    }),
+    Icons({
+      compiler: 'vue3',
+      autoInstall: true,
+      customCollections: {
+        agh: FileSystemIconLoader('./src/assets/images/icons')
+      }
+    }),
   ]
 })
