9df6c97730b533fa940bfc116c9956015d4ec06d
aries0d0f <aries0d0f@gmail.com>
2022-07-22 16:46:05 +0800

    feat(view): Implement `MainContainer` component.

---
+++ src/components/Layout/MainContainer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ScreenTone from '@/components/Layout/ScreenTone.vue';
+</script>
+
+<template>
+  <main :class="$style['container-main']">
+    <ScreenTone />
+  </main>
+</template>
+
+<style module lang="scss" scoped>
+@use '@/assets/styles/scss/components/container.module.scss' as container;
+</style>
