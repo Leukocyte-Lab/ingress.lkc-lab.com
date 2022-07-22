e9de4ec8855f234e3d3d2e43081882618c2f47a9
aries0d0f <aries0d0f@gmail.com>
2022-07-22 12:33:34 +0800

    build: Setup basic config, enable vue reactivity transform feature.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { join } from 'path';
+import Vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@': `${join(__dirname, 'src')}`
+    }
+  },
+  plugins: [
+    Vue({
+      reactivityTransform: true
+    }),
+    splitVendorChunkPlugin(),
+  ]
 })
