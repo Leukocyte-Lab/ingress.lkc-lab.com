d4ddf35a0112a481942e4225089cfae537817f30
aries0d0f <aries0d0f@gmail.com>
2022-07-22 18:44:32 +0800

    feat(view): Pass navbar items from `MainContainer`.

--- src/components/Layout/MainContainer.vue
+++ src/components/Layout/MainContainer.vue
@@ -1,9 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import type { AtLeastOne } from '@/shared/types/optional';
+
+const navbarItems = ref<
+  AtLeastOne<
+    {
+      name: string;
+      path: string;
+      hook: string;
+    },
+    'hook' | 'path'
+  >[]
+>([
+  {
+    name: '關於 LKC',
+    hook: 'about',
+  },
+  {
+    name: '產品與服務',
+    path: 'products',
+  },
+  {
+    name: '徵才資訊',
+    path: 'careers',
+  },
+  {
+    name: '聯絡我們',
+    path: 'contact',
+  },
+]);
+</script>
 
 <template>
   <main :class="$style['container-main']">
     <ScreenTone />
-    <NavBar />
+    <NavBar :items="navbarItems" />
   </main>
 </template>
 
