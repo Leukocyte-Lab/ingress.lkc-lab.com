c1634eaf106e4ca96559399db23106de1a169b10
aries0d0f <aries0d0f@gmail.com>
2022-07-23 19:55:01 +0800

    refactor(shared): Rewrite `Vector` util, add new compute methods.

--- src/shared/common/vector.ts
+++ src/shared/common/vector.ts
@@ -1,8 +1,14 @@
 import type { Vector } from '@/shared/types/vector';
 
-export const delta = <T extends string>(
-  a: Vector<T>,
-  b: Vector<T>,
-  axis: keyof Vector<T>,
-  returnVector = false
-): number => (returnVector ? a[axis] - b[axis] : Math.abs(a[axis] - b[axis]));
+export const vec2 = {
+  delta: (a: Vector<'x' | 'y'>, b: Vector<'x' | 'y'>): number =>
+    Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2)),
+  add: (a: Vector<'x' | 'y'>, b: Vector<'x' | 'y'>): Vector<'x' | 'y'> => ({
+    x: a.x + b.x,
+    y: a.y + b.y,
+  }),
+  minus: (a: Vector<'x' | 'y'>, b: Vector<'x' | 'y'>): Vector<'x' | 'y'> => ({
+    x: a.x - b.x,
+    y: a.y - b.y,
+  }),
+};
