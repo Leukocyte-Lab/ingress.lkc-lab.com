e9da078148f03f129f60c57640f7d4a691d6d021
aries0d0f <aries0d0f@gmail.com>
2022-07-25 09:19:30 +0800

    feat(view): Implement `SpotlightBox` component.

---
+++ src/components/Layout/SpotlightBox.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    wrapperComponent?: typeof import('*.vue') | string;
+  }>(),
+  {
+    wrapperComponent: 'div',
+  }
+);
+</script>
+
+<template>
+  <component
+    :is="wrapperComponent"
+    :class="[
+      $style['spotlight'],
+      $style['container--column'],
+      $style['inline'],
+      $style['center'],
+    ]"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<style module lang="scss" scoped>
+@use '@/assets/styles/scss/components/spotlight.module.scss' as spotlight;
+@use '@/assets/styles/scss/components/container.module.scss' as container;
+</style>
