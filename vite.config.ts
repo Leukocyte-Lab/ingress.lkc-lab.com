bfa43f6b9a5527122f6d428a58fae830c3ef9317
aries0d0f <aries0d0f@gmail.com>
2022-07-25 09:18:26 +0800

    build: Setup markdown plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite'
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
 import { join } from 'path';
 
 // Plugins
@@ -8,76 +8,92 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import Icons from 'unplugin-icons/vite';
 import IconsResolver from 'unplugin-icons/resolver';
+import Markdown from 'vite-plugin-vue-markdown';
 import Vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => defineConfig({
-  resolve: {
-    alias: {
-      '@': `${join(__dirname, 'src')}`
-    }
-  },
-  plugins: [
-    Vue({
-      reactivityTransform: true
-    }),
-    splitVendorChunkPlugin(),
-    AutoImport({
-      resolvers: [
-        IconsResolver({
-          prefix: 'icon',
-          alias: {
-            fab: 'fa-brands',
-            fas: 'fa-solid',
-            far: 'fa-regular'
-          }
-        })
-      ]
-    }),
-    Components({
-      dts: true,
-      resolvers: [
-        IconsResolver({
-          prefix: 'icon',
-          alias: {
-            fab: 'fa-brands',
-            fas: 'fa-solid',
-            far: 'fa-regular'
-          },
-          customCollections: ['agh']
-        })
-      ]
-    }),
-    Icons({
-      compiler: 'vue3',
-      autoInstall: true,
-      customCollections: {
-        agh: FileSystemIconLoader('./src/assets/images/icons')
-      }
-    }),
-    VitePluginFonts({
-      typekit: {
-        id: loadEnv(mode, process.cwd(), '')?.VITE_TYPEKIT_ID,
+export default ({ mode }) =>
+  defineConfig({
+    resolve: {
+      alias: {
+        '@': `${join(__dirname, 'src')}`,
       },
-      google: {
-        families: [
-          {
-            name: 'Noto Sans TC',
-            styles: 'wght@100;400;500',
-            defer: true,
-          },
+    },
+    plugins: [
+      Vue({
+        include: [/\.vue$/, /\.md$/],
+        reactivityTransform: true,
+      }),
+      Markdown({
+        markdownItOptions: {
+          breaks: true,
+          html: true,
+          linkify: true,
+          typographer: true,
+        },
+        markdownItSetup(md) {
+          md.use(require('markdown-it-anchor'));
+          md.use(require('markdown-it-attrs'));
+        },
+        wrapperClasses: 'article',
+      }),
+      splitVendorChunkPlugin(),
+      AutoImport({
+        resolvers: [
+          IconsResolver({
+            prefix: 'icon',
+            alias: {
+              fab: 'fa-brands',
+              fas: 'fa-solid',
+              far: 'fa-regular',
+            },
+          }),
         ],
+      }),
+      Components({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: 'icon',
+            alias: {
+              fab: 'fa-brands',
+              fas: 'fa-solid',
+              far: 'fa-regular',
+            },
+            customCollections: ['agh'],
+          }),
+        ],
+      }),
+      Icons({
+        compiler: 'vue3',
+        autoInstall: true,
+        customCollections: {
+          agh: FileSystemIconLoader('./src/assets/images/icons'),
+        },
+      }),
+      VitePluginFonts({
+        typekit: {
+          id: loadEnv(mode, process.cwd(), '')?.VITE_TYPEKIT_ID,
+        },
+        google: {
+          families: [
+            {
+              name: 'Noto Sans TC',
+              styles: 'wght@100;400;500',
+              defer: true,
+            },
+          ],
+        },
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            dompurify: ['dompurify'],
+            iconify: ['@iconify/iconify'],
+          },
+        },
       },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          dompurify: ['dompurify'],
-          iconify: ['@iconify/iconify']
-        }
-      }
-    }
-  },
-})
+    },
+  });
