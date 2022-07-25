a0e14441ea847dd70d0d4096dccb529afeacf911
aries0d0f <aries0d0f@gmail.com>
2022-07-25 12:28:38 +0800

    feat(view): Implement `GeneralButton`.

---
+++ src/components/Element/GeneralButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+
+withDefaults(
+  defineProps<{
+    text: string;
+    link: string;
+    icon: string;
+    iconPosition?: 'left' | 'right';
+  }>(),
+  {
+    iconPosition: 'left',
+  }
+);
+</script>
+
+<template>
+  <a :class="{ flip: iconPosition === 'right' }" class="button" :href="link">
+    <Icon :icon="icon" />
+    <span class="text">{{ text }}</span>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/scss/components/palette.module.scss' as palette;
+@use '@/assets/styles/scss/components/button.module.scss' as button;
+
+.button {
+  @include button.use(
+    $font-size: 18px,
+    $palette-text: palette.$white,
+    $palette-text-link-hover: palette.$white,
+    $palette-bg: palette.$purple-0
+  );
+}
+</style>
