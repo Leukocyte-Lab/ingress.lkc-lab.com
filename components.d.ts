1ec6f782cd5ba6259c2fa365b47d36a4103dc0f2
aries0d0f <aries0d0f@gmail.com>
2022-07-25 13:45:27 +0800

    chores: Track auto-import generated type declaration file.

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,8 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     GeneralButton: typeof import('./src/components/Element/GeneralButton.vue')['default']
     MainContainer: typeof import('./src/components/Layout/MainContainer.vue')['default']
+    MapBox: typeof import('./src/components/Exclusive/MapBox.vue')['default']
+    MapViewer: typeof import('./src/components/Exclusive/MapViewer.vue')['default']
     NavBar: typeof import('./src/components/Element/NavBar.vue')['default']
     NeonTexture: typeof import('./src/components/Texture/NeonTexture.vue')['default']
     ScreenTone: typeof import('./src/components/Layout/ScreenTone.vue')['default']
