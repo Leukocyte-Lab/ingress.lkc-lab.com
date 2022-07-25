7c4721217a09eba7f9a63b7b4054688f6a0d8627
aries0d0f <aries0d0f@gmail.com>
2022-07-25 11:05:21 +0800

    feat(view): Add `NeonTexture` component.

---
+++ src/components/Texture/NeonTexture.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <filter
+        id="neon-effect"
+        color-interpolation-filters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset dx="1" dy="3" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.4 0" />
+        <feBlend in2="shape" result="effect1_innerShadow" />
+      </filter>
+    </defs>
+  </svg>
+</template>
