237b1d616689c2c0e270eec8aa4a95edb6567179
aries0d0f <aries0d0f@gmail.com>
2022-07-26 08:52:20 +0800

    feat(view): Adjust breakpoint.

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@ import { provide } from 'vue';
 import { useBreakpoints } from '@vueuse/core';
 
 const breakpoints = useBreakpoints({
-  mobile: 768,
-  tablet: 1024,
-  desktop: 1440,
-  large: 1920,
+  mobile: 768.1,
+  tablet: 1024.1,
+  desktop: 1440.1,
+  large: 1920.1,
 });
 const isMobile = breakpoints.smaller('mobile');
 
